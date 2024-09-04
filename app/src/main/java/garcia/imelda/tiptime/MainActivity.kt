b53de23b099aced991796f41f591c699bbd0bf64
Castellanos Imelda <imelda.castellanos09@gmail.com>
2024-09-04 00:00:03 -0600

    Pendiente por resolver un pequeño error

--- app/src/main/java/garcia/imelda/tiptime/MainActivity.kt
+++ app/src/main/java/garcia/imelda/tiptime/MainActivity.kt
@@ -1,6 +1,9 @@
 package garcia.imelda.tiptime
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,41 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        enableEdgeToEdge()
+
+        //EXTRAE LOS DATOS POR ID EN EL ARCHIVO XML
+        val tipRadioGroup = findViewById<RadioGroup>(R.id.tip_options)
+        val totalAmount = findViewById<EditText>(R.id.cost_of_service)
+
+        // SE REALIZA UN LISTENER SEGUN EL BOTON QUE ESTE SELECCIONADO
+        //IDENTIFICANDO POR MEDIO DE LO ID´S
+        tipRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val tipPercentage = when (checkedId) {
+                R.id.option_twenty_percent -> 0.20
+                R.id.option_fifteen_percent -> 0.15
+                R.id.option_eighteen_percent -> 0.18
+                else -> 0.0 // VALOR POR DEFAULT
+            }
+
+            //SE CALCULA EL TOTAL DE PROPINAS MULTIPLICANDO EL TOTAL DE LA CUENTA POR EL
+            //PORCENTAJE SELECCIONADO
+
+            val tipAmount = totalAmount * tipPercentage
+
+
+            //SE MUESTRA EN EL CAMPO DEL TEXT VIEW ENCONTRADO MEDIANTE ID
+            //EL VALOR PREVIAMENTE CALCULADO "TIPAMOUNT"
+            val tipTextView = findViewById<TextView>(R.id.tip_result)
+            tipTextView.text = getString(R.string.tip_amount, tipAmount)
+
+
+            setContentView(R.layout.activity_main)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
